3da61de6aa09b585fbb50445cfe64144007dc854
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2018-07-24 12:36:59 +0100

    Completed Project 1

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,19 @@
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return; //skip if not a transform
+    this.classList.remove('playing');
+}
+
+function playSound(element) {
+    const audio = document.querySelector(`audio[data-key="${element.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${element.keyCode}"]`);
+    
+    if(!audio) return; // Stop function from running
+    audio.currentTime = 0; // Rewind to the start
+    audio.play();
+    key.classList.add('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
