fb9654a772ac29a5d64f54faa7c68c4cffea2fb5
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2018-08-13 16:15:57 +0100

    Completed Project 24

---
+++ 23 - Speech Synthesis/script.js
@@ -0,0 +1,18 @@
+const synth = window.speechSynthesis;
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})`)
+    .join('');
+}
+
+window.speechSynthesis.addEventListener('voiceschanged', populateVoices);
