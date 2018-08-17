605cce9d8aac4c11848f98672f36e918ccc40046
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2018-08-17 14:30:16 +0100

    Completed Project 27

---
+++ 27 - Click and Drag/script.js
@@ -0,0 +1,29 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (event) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = event.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (event) => {
+  if (!isDown) return;  // stop the fn from running
+  event.preventDefault();
+  const x = event.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+});
